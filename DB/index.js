f92e3065713569031a2979ed744a98373598469c
Vannername <vannername@gmail.com>
2022-02-18 14:49:02 +0200

    #2

--- DB/index.js
+++ DB/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 
 const Logger = require("../Logger")
-const authRouter = require("./Routers/authRouter")
+const authRouter = require("./Routers/DBRouter")
 const errorMiddleware = require("./Middlewares/errorMiddleware")
 
 const PORT = process.env.DB_PORT
