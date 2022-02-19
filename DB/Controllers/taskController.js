05d048e3b36038e5aa5ad02e23da3f08e3ce32bc
Dorideo <mikhailvarga2005@gmail.com>
2022-02-19 15:57:10 +0200

    Huyna

--- DB/Controllers/taskController.js
+++ DB/Controllers/taskController.js
@@ -1,13 +1,20 @@
 const TaskService = require("../Service/taskService")
 const TaskDTO = require("../../DTOs/TaskDTO")
+const File = require("node")
 
 class TaskController{
     async addTask(req, res, next){
-        const {due_to, description, file_names} = req.body;
+        var {due_to, description, file_names} = req.body;
         const fileList = [];
 
         for(let i = 0; i < file_names.length; i++){
-            fileList.push(new File([], file_names[i]))
+            for(let j = 0; j < file_names[i].length; j++){
+                if(file_names[i][j] == '|'){
+                    file_names[i].replace('|', '/');
+                }
+            }
+            const file = new File([], file_names[i]);
+            fileList.push(file);
         }
 
         const taskDTO = new TaskDTO(0, due_to, description, fileList, []);
